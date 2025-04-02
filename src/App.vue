634a37a8e58390d7cc92b34e742e15e10a575976
Adrián Maza <adri_mv1@hotmail.com>
2025-04-02 10:36:41 +0200

    project created

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { ref } from 'vue';
+import HeaderComponent from './components/HeaderComponent.vue';
+
+let text = "Soy un texto que viene de una variable";
+const counter = ref(0);
+
+setInterval(() => {
+  counter.value = counter.value + 1;
+}, 1000);
+</script>
+
+<template>
+  <HeaderComponent />
+
+  <h2>{{ counter }}</h2>
+
+  <h2 class="title">Bienvenidos a mi Web</h2>
+
+  <p>
+    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Placeat dolorum
+    tenetur magnam illum laborum nemo accusamus debitis? Delectus ipsam
+    molestias voluptate repellat ab, fuga animi, fugit, maxime labore aliquid
+    quaerat!
+  </p>
+  <p>{{ text }}</p>
+</template>
+
+<style>
+.title {
+  background-color: yellow;
+}
+</style>
---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,42 @@
+<script setup>
+</script>
+
+<template>
+  <header class="header">
+    <h1 class="header-title">Hola a Todos</h1>
+    <nav>
+      <ul class="nav-list">
+        <li><a href="#" class="link">Home</a></li>
+        <li><a href="#" class="link">Blog</a></li>
+        <li><a href="#" class="link">Contact Us</a></li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<style>
+.header {
+  background-color: #000022;
+  color: white;
+  padding: 20px;
+
+  .header-title {
+    margin: 0;
+    margin-bottom: 30px;
+    text-align: center;
+  }
+
+  .nav-list {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+    display: flex;
+    justify-content: space-around;
+
+    .link {
+      color: white;
+      text-decoration: none;
+    }
+  }
+}
+</style>
